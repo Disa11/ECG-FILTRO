fdccb0834c2eb4b22014017702d91857322b5708
Disa11 <111477455+Disa11@users.noreply.github.com>
2024-06-17 17:40:18 -0600

    Add files via upload

--- sys final/loadMatFile.m
+++ sys final/loadMatFile.m
@@ -1,4 +1,4 @@
-function [val, ecg_final, t, f, F, ecg_clean2] = loadMatFile(dir)
+function [val, ecg_final3, t, f, F, ecg_cleanf] = loadMatFile(dir)
     % -- Cargar la señal -- %
     data = load(dir);
     val = data.val;
@@ -7,53 +7,133 @@ function [val, ecg_final, t, f, F, ecg_clean2] = loadMatFile(dir)
     G = 30; %1mv
     ecg_mv = val / G; % Amplitud ajustada
 
-    % Vector de tiempo Fs
-    Fs = 250; % [Hz]
-    Ts = 1 / Fs; % [s]
+%vector de tiempo Fs
+Fs = 500; %[Hz]
+Ts = 1/Fs; %[s]
 
-    N = length(val);
-    vect = (1:1:N);
-    t = vect * Ts;
+N = length(val);
+vect = (1:1:N);
+t = vect*Ts;
 
-    ecg_final = (ecg_mv - mean(ecg_mv)) / std(ecg_mv); % Señal centrada en 0
+ecg_final = (ecg_mv - mean(ecg_mv)/std(ecg_mv))%señal centrada en 0%
 
-    % FFT
-    F = fft(val); % Comando de la transformada de Fourier
-    F = abs(F); % Calcula el valor absoluto de la señal
-    F = F(1:ceil(end / 2)); % Partición de la señal a la mitad
-    F = F / max(F); % Ajuste de la magnitud de 0 a 1
 
-    % Vector de frecuencia
-    L = length(F);
-    f = (1:1:L) * ((Fs / 2) / L);
+%FFT-------------------------------------------------------%
 
-    % Filtro tipo FIR
-    orden = 200;
-    limi = 20;
-    lims = 28;
+F = fft(val);%comando de la transformada de fourier%
+F = abs(F);%calcula el valor absoluto de la señal%
+%particion de la señal a la mitad y se agarra con ceil el mas aproximado
+F = F(1:ceil(end/2));
+%ajuste de la magnitud de 0 a 1%
+F = F/max(F);
 
-    % Normalizar los límites
-    limi_n = limi / (Fs / 2);
-    lims_n = lims / (Fs / 2);
+%vector de frecuencia
+L = length(F);
+f = (1:1:L) * ((Fs/2)/L);
 
-    % Creación del filtro
-    a = 1;
-    b = fir1(orden, [limi_n lims_n], 'stop');
+%---------------------------------------------------------------------------
+%filto de filterDesigner%
+%Bandstop Filter%
 
-    % Filtrar la señal
-    ecg_clean = filtfilt(b, a, F); % Para corroborar donde está el filtro escribe freqz(b, a)
+Fs = 500;  % Sampling Frequency
 
-    % Filtro de filterDesigner
-    Fs = 250;  % Sampling Frequency
-    N = 10;  % Order
-    Fc1 = 20;  % First Cutoff Frequency
-    Fc2 = 26;  % Second Cutoff Frequency
+N   = 10;  % Order
+Fc1 = 45;  % First Cutoff Frequency
+Fc2 = 55;  % Second Cutoff Frequency
 
-    % Construct an FDESIGN object and call its BUTTER method.
-    h = fdesign.bandstop('N,F3dB1,F3dB2', N, Fc1, Fc2, Fs);    
-    Hd = design(h, 'butter');
+% Construct an FDESIGN object and call its BUTTER method.
+h  = fdesign.bandstop('N,F3dB1,F3dB2', N, Fc1, Fc2, Fs);
+Hd = design(h, 'butter');
 
-    ecg_clean2 = filter(Hd, F);
+ecg_final2 = filter(Hd,ecg_final);%en el tiempo
+
+ecg_cleanf = filter(Hd,F);%en la frecuencia
+ecg_cleanf = abs(ecg_cleanf);
+
+%--------------------------------------------------------------------------
+% Equiripple Lowpass filter designed using the FIRPM function.
+%Lowpass Filter%
+
+% All frequency values are in Hz.
+Fs = 500;  % Sampling Frequency
+
+N     = 10;  % Order
+Fpass = 49.5;  % Passband Frequency
+Fstop = 51.5;  % Stopband Frequency
+Wpass = 1;   % Passband Weight
+Wstop = 1;   % Stopband Weight
+dens  = 20;  % Density Factor
+
+% Calculate the coefficients using the FIRPM function.
+b  = firpm(N, [0 Fpass Fstop Fs/2]/(Fs/2), [1 1 0 0], [Wpass Wstop], ...
+           {dens});
+Hd2 = dfilt.dffir(b);
+
+%------------------------------------------------------------------------
+% Equiripple Highpass filter designed using the FIRPM function.
+
+% All frequency values are in Hz.
+Fs = 500;  % Sampling Frequency
+
+N     = 10;  % Order
+Fstop = 49.5;  % Stopband Frequency
+Fpass = 51.5;  % Passband Frequency
+Wstop = 1;   % Stopband Weight
+Wpass = 1;   % Passband Weight
+dens  = 20;  % Density Factor
+
+% Calculate the coefficients using the FIRPM function.
+b  = firpm(N, [0 Fstop Fpass Fs/2]/(Fs/2), [0 0 1 1], [Wstop Wpass], ...
+           {dens});
+Hd3 = dfilt.dffir(b);
+
+
+
+ecg_final3 = filter(Hd2,ecg_final2);
+
+ecg_cleanf = filter(Hd2,ecg_cleanf);
+ecg_cleanf = abs(ecg_cleanf);
+%-------------------------------------------------------------------------
+
+%ploteo de señales filtradas---------------------------------------------------------
+
+
+%extraccion de caracteristicas-----------------------
+
+
+ecg_final = ecg_final3;
+umbral_y = 6*mean(abs(ecg_final));
+umbral_x = 0.02*Fs;
+
+[PKS, LOCS] = findpeaks(ecg_final, 'MinpeakHeight',umbral_y,'minpeakDistance',umbral_x);
+% 
+% 
+% %ubicacion en el tiempo de los picos
+% R_loc_time = t(LOCS);
+% 
+% %calcular HRV
+% HRV = diff(R_loc_time);
+% 
+%  %RMSSD%
+%  %primer paso
+%  
+%  resta_RR = diff(HRV);
+%  
+%  %segundo paso
+%  resta_RR2 = resta_RR.^2;
+%  
+%  %tercer paso
+%  suma_resta_RR2 = sum(resta_RR2);
+%  
+%  %cuarto paso
+%  norm_resta_RR2 = suma_resta_RR2/lenght(resta_RR);
+%  
+% %ultimo paso
+% RMSSD = sqrt(norm_resta_RR2);
+% 
+% RMSSD_ms = RMSSD*1000;
+% 
+
 
     % Devuelve los datos necesarios para las gráficas
 end
--- sys final/proyecto.m
+++ sys final/proyecto.m
@@ -1,18 +1,16 @@
 clc;close all;clear;
 %cargar señal
-load('E:\MATLAB\sys final\rec_1m.mat');
+load('rec_1m.mat');
 %ajustar amplitud
 G = 30; %1mv
 ecg_mv = val/G;%amplitud ajustada
-
-figure;
 
 %plot(ecg_mv);
 %ylabel('Amplitud (mv)');
 %xlim([0 1000])
 
 %vector de tiempo Fs
-Fs = 250; %[Hz]
+Fs = 500; %[Hz]
 Ts = 1/Fs; %[s]
 
 N = length(val);
@@ -22,7 +20,7 @@ t = vect*Ts;
 ecg_final = (ecg_mv - mean(ecg_mv)/std(ecg_mv))%señal centrada en 0%
 
 
-%FFT%
+%FFT-------------------------------------------------------%
 
 F = fft(val);%comando de la transformada de fourier%
 F = abs(F);%calcula el valor absoluto de la señal%
@@ -34,63 +32,147 @@ F = F/max(F);
 %vector de frecuencia
 L = length(F);
 f = (1:1:L) * ((Fs/2)/L);
-%plot(f,F)
-%xlabel('Frecuencia(Hz)');
-%ylabel('Magnitud Normalizada');
-%title('ECG en frecuencia');
-
-%Filtro tipo FIR%
-%caracterizticas del filtro
-orden = 200;
-limi = 20;
-lims = 28;
-
-%normalizar los limites%
-limi_n = limi/(Fs/2);
-lims_n = lims/(Fs/2);
-
-%creacion del filtro%
-a = 1;
-b = fir1(orden,[limi_n lims_n], 'stop');
-
-%filtrar la señal%
-ecg_clean = filtfilt(b,a,F);%para corroborrar donde esta el filtro escribe freqz(b,a)
-
-subplot(2,2,1);
-plot(val);
-title('señal no filtrada');
-
-subplot(2,2,2);
-plot(t,ecg_final);
-ylabel('amplitud mv');
-xlabel ('tiempo s');
-title('señal filtrada');
-xlim([0 4])
-
-subplot(2,2,3)
-plot(f,F)
-xlabel('Frecuencia(Hz)');
-ylabel('Magnitud Normalizada');
-title('ECG en frecuencia');
 
 %---------------------------------------------------------------------------
 %filto de filterDesigner%
+%Bandstop Filter%
 
-Fs = 250;  % Sampling Frequency
+Fs = 500;  % Sampling Frequency
 
 N   = 10;  % Order
-Fc1 = 20;  % First Cutoff Frequency
-Fc2 = 26;  % Second Cutoff Frequency
+Fc1 = 45;  % First Cutoff Frequency
+Fc2 = 55;  % Second Cutoff Frequency
 
 % Construct an FDESIGN object and call its BUTTER method.
 h  = fdesign.bandstop('N,F3dB1,F3dB2', N, Fc1, Fc2, Fs);
 Hd = design(h, 'butter');
 
-ecg_clean2 = filter(Hd,F);
+ecg_final2 = filter(Hd,ecg_final);%en el tiempo
+
+ecg_cleanf = filter(Hd,F);%en la frecuencia
+ecg_cleanf = abs(ecg_cleanf);
+
+%--------------------------------------------------------------------------
+% Equiripple Lowpass filter designed using the FIRPM function.
+%Lowpass Filter%
+
+% All frequency values are in Hz.
+Fs = 500;  % Sampling Frequency
+
+N     = 10;  % Order
+Fpass = 49.5;  % Passband Frequency
+Fstop = 51.5;  % Stopband Frequency
+Wpass = 1;   % Passband Weight
+Wstop = 1;   % Stopband Weight
+dens  = 20;  % Density Factor
+
+% Calculate the coefficients using the FIRPM function.
+b  = firpm(N, [0 Fpass Fstop Fs/2]/(Fs/2), [1 1 0 0], [Wpass Wstop], ...
+           {dens});
+Hd2 = dfilt.dffir(b);
+
+%------------------------------------------------------------------------
+% Equiripple Highpass filter designed using the FIRPM function.
+
+% All frequency values are in Hz.
+Fs = 500;  % Sampling Frequency
+
+N     = 10;  % Order
+Fstop = 49.5;  % Stopband Frequency
+Fpass = 51.5;  % Passband Frequency
+Wstop = 1;   % Stopband Weight
+Wpass = 1;   % Passband Weight
+dens  = 20;  % Density Factor
+
+% Calculate the coefficients using the FIRPM function.
+b  = firpm(N, [0 Fstop Fpass Fs/2]/(Fs/2), [0 0 1 1], [Wstop Wpass], ...
+           {dens});
+Hd3 = dfilt.dffir(b);
+
+
+
+ecg_final3 = filter(Hd2,ecg_final2);
+
+ecg_cleanf = filter(Hd2,ecg_cleanf);
+ecg_cleanf = abs(ecg_cleanf);
+%-------------------------------------------------------------------------
+figure(1);
+subplot(2,2,1);
+plot(val);
+title('ECG');
+
+subplot(2,2,3)
+plot(f,F)
+xlabel('Frecuencia(Hz)');
+ylabel('Magnitudnormalizada');
+title('ECG en frecuencia');
+
+%ploteo de señales filtradas---------------------------------------------------------
+subplot(2,2,2);
+plot(t,ecg_final3);
+ylabel('amplitud mv');
+xlabel ('tiempo s');
+title('ECG filtrado FIR');
+xlim([0 2])
+
 
 subplot(2,2,4)
-plot(f,ecg_clean2)
+plot(f,ecg_cleanf)
 xlabel('frecuencia (Hz)');
 ylabel ('magnitud normalizada con filtro FIR');
-title('ECG en frecuencia');
+title('ECG en frecuencia FIR');
+
+
+%extraccion de caracteristicas-----------------------
+
+
+ecg_final = ecg_final3;
+umbral_y = 6*mean(abs(ecg_final));
+umbral_x = 0.02*Fs;
+
+[PKS, LOCS] = findpeaks(ecg_final, 'MinpeakHeight',umbral_y,'minpeakDistance',umbral_x);
+
+figure(2);
+hold on;
+plot(t,ecg_final);
+scatter(t(LOCS),PKS);
+ylabel('amplitud mv');
+xlabel ('tiempo s');
+title('ECG filtrado FIR');
+xlim([0 4])
+hold off;
+
+%ubicacion en el tiempo de los picos
+R_loc_time = t(LOCS);
+
+%calcular HRV
+HRV = diff(R_loc_time);
+
+ %RMSSD%
+ %primer paso
+ 
+ resta_RR = diff(HRV);
+ 
+ %segundo paso
+ resta_RR2 = resta_RR.^2;
+ 
+ %tercer paso
+ suma_resta_RR2 = sum(resta_RR2);
+ 
+ %cuarto paso
+ norm_resta_RR2 = suma_resta_RR2/lenght(resta_RR);
+ 
+%ultimo paso
+RMSSD = sqrt(norm_resta_RR2);
+
+RMSSD_ms = RMSSD*1000;
+
+
+
+
+
+
+
+
+
 
